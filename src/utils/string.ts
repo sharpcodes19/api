539cdc0f9131b95d36d312a6d465d96555987cdf
Jae Jarabe <jaesharp19@gmail.com>
2024-02-16 16:22:51 +0800

    color

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -41,3 +41,19 @@ export const uriToBlobAsync = (uri: string) =>
 		xhr.open('GET', uri, true)
 		xhr.send(null)
 	})
+
+export const hexToRGBAColor = (hex: string, alpha?: number) => {
+	if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+		let c = hex.substring(1).split('')
+		if (c.length == 3) {
+			c = [c[0], c[0], c[1], c[1], c[2], c[2]]
+		}
+		const rgba: any = '0x' + c.join('')
+		return (
+			'rgba(' +
+			[(rgba >> 16) & 255, (rgba >> 8) & 255, rgba & 255].join(',') +
+			`,${alpha ?? 1})`
+		)
+	}
+	throw new Error('Bad Hex')
+}
